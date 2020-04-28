e7a4d8ddf9f0834016da953a69901fba242055b7
tamberg <tamberg@yaler.net>
2020-04-28 11:29:58 +0200

    Split HumiTempBrick to support multiple encodings.

--- Java/src/main/java/ch/fhnw/imvs/bricks/sensors/HumiTempBrick.java
+++ Java/src/main/java/ch/fhnw/imvs/bricks/sensors/HumiTempBrick.java
@@ -5,26 +5,70 @@ package ch.fhnw.imvs.bricks.sensors;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.nio.ByteBuffer;
+import java.util.Base64;
 
 import ch.fhnw.imvs.bricks.core.Brick;
 import ch.fhnw.imvs.bricks.core.Proxy;
 
-public final class HumiTempBrick extends Brick {
-    private HumiTempBrick(Proxy proxy, String brickID) {
+// /* package */ final class ThingSpeakHumiTempBrick extends HumiTempBrick {}
+
+/* package */ final class TtnBasicHumiTempBrick extends HumiTempBrick {
+    /* package */ TtnBasicHumiTempBrick(Proxy proxy, String brickID) {
+        super(proxy, brickID);
+    }
+
+    private String getValueOf(String json, String name) {
+        // TODO: error handling
+        int p = json.indexOf(name);
+        int q = json.indexOf('"', p + 1);
+        q = json.indexOf('"', q + 1);
+        int r = json.indexOf('"', q + 1);
+        return json.substring(q + 1, r);
+    }
+
+    @Override
+    protected void setCurrentPayload(byte[] payload) {
+        String mqttPayload = new String(payload, StandardCharsets.UTF_8);
+        String ttnPayloadBase64 = getValueOf(mqttPayload, "payload_raw");
+        byte[] ttnPayload = Base64.getDecoder().decode(ttnPayloadBase64);
+        ByteBuffer buf = ByteBuffer.wrap(ttnPayload);
+        super.setBatteryLevel(100); // TODO
+        super.setHumidity(buf.getShort() / 100.0);
+        super.setTemperature(buf.getShort() / 100.0);
+    }
+
+    @Override
+    protected byte[] getTargetPayload(boolean mock) {
+        byte[] payload = null;
+        if (mock) {
+            // TODO: move to getMock...() in base class?
+            short mockBatt = (short) (Math.random() * 99 + 1);
+            short mockHumi = (short) (100 * (Math.random() * 99 + 1));
+            short mockTemp = (short) (100 * (Math.random() * 50 + 1)); 
+            ByteBuffer buf = ByteBuffer.allocate(4);
+            // TODO: put mockBatt
+            buf.putShort(mockHumi);
+            buf.putShort(mockTemp);
+            byte[] ttnPayload = buf.array();
+            String ttnPayloadBase64 = Base64.getEncoder().encodeToString(ttnPayload);
+            String mqttPayload = "{\"payload_raw\":\"" + ttnPayloadBase64 + "\"}";     
+            try {
+                payload = mqttPayload.getBytes("UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return payload;
+    }
+}
+
+/* package */ final class Utf8HumiTempBrick extends HumiTempBrick {
+    /* package */ Utf8HumiTempBrick(Proxy proxy, String brickID) {
         super(proxy, brickID);
     }
 
     private final String SEPARATOR = ";";
-    private volatile double currentHumi;
-    private volatile double currentTemp;
-
-    public double getHumidity() {
-        return currentHumi;
-    }
-
-    public double getTemperature() {
-        return currentTemp;
-    }
 
     @Override
     protected void setCurrentPayload(byte[] payload) {
@@ -32,8 +76,8 @@ public final class HumiTempBrick extends Brick {
             String message = new String(payload, StandardCharsets.UTF_8);
             String[] parts = message.split(SEPARATOR);
             super.setBatteryLevel(Integer.parseInt(parts[0]));
-            currentHumi = Double.parseDouble(parts[1]);
-            currentTemp = Double.parseDouble(parts[2]);
+            super.setHumidity(Double.parseDouble(parts[1]));
+            super.setTemperature(Double.parseDouble(parts[2]));
         } catch(NumberFormatException e) {
             e.printStackTrace();
         }
@@ -43,24 +87,43 @@ public final class HumiTempBrick extends Brick {
     protected byte[] getTargetPayload(boolean mock) {
         byte[] payload = null;
         if (mock) {
-            int mockBatt = (int) (Math.random() * 99 + 1);
+            // TODO: move to getMock...() in base class?
+            int mockBatt = (short) (Math.random() * 99 + 1);
             double mockHumi = Math.random() * 99 + 1;
-            double mockTemp = Math.random() * 50 + 1;
+            double mockTemp = Math.random() * 50 + 1; 
             try {
-                String payloadString = 
-                    Integer.toString(mockBatt) + SEPARATOR +
-                    Double.toString(mockHumi) + SEPARATOR + 
-                    Double.toString(mockTemp);
-                payload = payloadString.getBytes("UTF-8");
+                String utf8Payload = 
+                     Integer.toString(mockBatt) + SEPARATOR +
+                     Double.toString(mockHumi) + SEPARATOR + 
+                     Double.toString(mockTemp);
+                payload = utf8Payload.getBytes("UTF-8");
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
         }
         return payload;
     }
+}
+
+// Disadvantage: this class has to be public, but it's also not final.
+
+public abstract class HumiTempBrick extends Brick {
+    protected HumiTempBrick(Proxy proxy, String brickID) {
+        super(proxy, brickID);
+    }
+
+    private volatile double currentHumi;
+    private volatile double currentTemp;
+
+    protected void setHumidity(double humi) { currentHumi = humi; }
+    protected void setTemperature(double temp) { currentTemp = temp; }
+
+    public double getHumidity() { return currentHumi; }
+    public double getTemperature() { return currentTemp; }
 
     public static HumiTempBrick connect(Proxy proxy, String brickID) {
-        HumiTempBrick brick = new HumiTempBrick(proxy, brickID);
+        //HumiTempBrick brick = new TtnBasicHumiTempBrick(proxy, brickID); // TODO: dynamic
+        HumiTempBrick brick = new Utf8HumiTempBrick(proxy, brickID); // TODO: dynamic
         brick.connect();
         return brick;
     }
