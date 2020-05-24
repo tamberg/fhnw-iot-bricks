f235ef9b6898b80aa5f185c525881d7517b0a1ac
tamberg <tamberg@yaler.net>
2020-05-24 10:54:54 +0200

    Refactored.

--- Java/src/main/java/ch/fhnw/imvs/bricks/sensors/HumiTempBrick.java
+++ Java/src/main/java/ch/fhnw/imvs/bricks/sensors/HumiTempBrick.java
@@ -13,27 +13,15 @@ import ch.fhnw.imvs.bricks.core.Proxy;
 
 // /* package */ final class ThingSpeakHumiTempBrick extends HumiTempBrick {}
 
-/* package */ final class TtnBasicHumiTempBrick extends HumiTempBrick {
-    /* package */ TtnBasicHumiTempBrick(Proxy proxy, String brickID) {
+/* package */ final class LppHumiTempBrick extends HumiTempBrick {
+    /* package */ LppHumiTempBrick(Proxy proxy, String brickID) {
         super(proxy, brickID);
-    }
-
-    private String getValueOf(String json, String name) {
-        // TODO: error handling
-        int p = json.indexOf(name);
-        int q = json.indexOf('"', p + 1);
-        q = json.indexOf('"', q + 1);
-        int r = json.indexOf('"', q + 1);
-        return json.substring(q + 1, r);
     }
 
     @Override
     protected void setCurrentPayload(byte[] payload) {
-        String mqttPayload = new String(payload, StandardCharsets.UTF_8);
-        String ttnPayloadBase64 = getValueOf(mqttPayload, "payload_raw");
-        byte[] ttnPayload = Base64.getDecoder().decode(ttnPayloadBase64);
-        ByteBuffer buf = ByteBuffer.wrap(ttnPayload);
-        super.setBatteryLevel(100); // TODO
+        ByteBuffer buf = ByteBuffer.wrap(payload);
+        super.setBatteryLevel(buf.getShort());
         super.setHumidity(buf.getShort() / 100.0);
         super.setTemperature(buf.getShort() / 100.0);
     }
@@ -42,22 +30,14 @@ import ch.fhnw.imvs.bricks.core.Proxy;
     protected byte[] getTargetPayload(boolean mock) {
         byte[] payload = null;
         if (mock) {
-            // TODO: move to getMock...() in base class?
             short mockBatt = (short) (Math.random() * 99 + 1);
             short mockHumi = (short) (100 * (Math.random() * 99 + 1));
             short mockTemp = (short) (100 * (Math.random() * 50 + 1)); 
             ByteBuffer buf = ByteBuffer.allocate(4);
-            // TODO: put mockBatt
+            buf.putShort(mockBatt);
             buf.putShort(mockHumi);
             buf.putShort(mockTemp);
-            byte[] ttnPayload = buf.array();
-            String ttnPayloadBase64 = Base64.getEncoder().encodeToString(ttnPayload);
-            String mqttPayload = "{\"payload_raw\":\"" + ttnPayloadBase64 + "\"}";     
-            try {
-                payload = mqttPayload.getBytes("UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
+            payload = buf.array();
         }
         return payload;
     }
@@ -122,8 +102,8 @@ public abstract class HumiTempBrick extends Brick {
     public double getTemperature() { return currentTemp; }
 
     public static HumiTempBrick connect(Proxy proxy, String brickID) {
-        //HumiTempBrick brick = new TtnBasicHumiTempBrick(proxy, brickID); // TODO: dynamic
-        HumiTempBrick brick = new Utf8HumiTempBrick(proxy, brickID); // TODO: dynamic
+        HumiTempBrick brick = new LppHumiTempBrick(proxy, brickID); // TODO: dynamic
+        //HumiTempBrick brick = new Utf8HumiTempBrick(proxy, brickID); // TODO: dynamic
         brick.connect();
         return brick;
     }
