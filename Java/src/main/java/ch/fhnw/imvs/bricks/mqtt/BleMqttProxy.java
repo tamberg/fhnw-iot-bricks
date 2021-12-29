8312b469824455252dc218dcb88ddacff529f00f
tamberg <tamberg@yaler.net>
2021-12-29 15:44:30 +0100

    Update.

--- Java/src/main/java/ch/fhnw/imvs/bricks/mqtt/BleMqttProxy.java
+++ Java/src/main/java/ch/fhnw/imvs/bricks/mqtt/BleMqttProxy.java
@@ -3,16 +3,11 @@
 
 package ch.fhnw.imvs.bricks.mqtt;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import ch.fhnw.imvs.bricks.core.Brick;
 import ch.fhnw.imvs.bricks.core.Proxy;
-
-// BleMqttProxy knows how to (un)pack TTN LoRaWAN / MQTT payload.
 
 public final class BleMqttProxy extends Proxy {
     private BleMqttProxy(MqttConfig config) {
@@ -32,26 +27,14 @@ public final class BleMqttProxy extends Proxy {
         mqttService.connect();
     }
 
-    private String getValueOf(String json, String name) {
-        int p = json.indexOf(name);
-        int q = json.indexOf('"', p + 1);
-        q = json.indexOf('"', q + 1);
-        int r = json.indexOf('"', q + 1);
-        return json.substring(q + 1, r);
-    }
-
     @Override
     public void connectBrick(Brick brick) {
         String topic = mqttConfig.getSubscribeTopic(brick.getID());
         IMqttMessageListener listener = new IMqttMessageListener() {
             public void messageArrived(String topic, MqttMessage message) throws Exception {
-                //System.out.printf("messageArrived topic = \"%s\", payload = \"%s\"\n", topic, message);
-                byte[] ttnMqttPayloadBytes = message.getPayload();
-                String ttnMqttPayloadJson = new String(ttnMqttPayloadBytes, StandardCharsets.UTF_8);
-                String ttnLoRaPayloadBase64 = BleMqttProxy.this.getValueOf(
-                    ttnMqttPayloadJson, "payload_raw");
-                byte[] ttnLoRaPayloadBytes = Base64.getDecoder().decode(ttnLoRaPayloadBase64);
-                BleMqttProxy.this.setPendingPayload(brick, ttnLoRaPayloadBytes);
+                System.out.printf("messageArrived topic = \"%s\", payload = \"%s\"\n", topic, message);
+                byte[] payload = message.getPayload();
+                BleMqttProxy.this.setPendingPayload(brick, payload);
             }
         };
         mqttService.subscribe(topic, listener);
@@ -60,23 +43,10 @@ public final class BleMqttProxy extends Proxy {
 
     @Override
     protected void syncBrick(Brick brick) {
-        byte[] ttnLoRaPayloadBytes = super.getTargetPayload(brick, false); // not a mock
-        String ttnLoRaPayloadBase64 = Base64.getEncoder().encodeToString(ttnLoRaPayloadBytes);
-        String ttnMqttPayloadJson = 
-            "{" +
-                "\"port\": 1," +
-                "\"confirmed\": false," +
-                "\"payload_raw\": \"" + ttnLoRaPayloadBase64 + "\"," +
-            "}";
-        byte[] ttnMqttPayloadBytes = null;
-        try {
-            ttnMqttPayloadBytes = ttnMqttPayloadJson.getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        byte[] payload = super.getTargetPayload(brick, false); // not a mock
         String topic = mqttConfig.getPublishTopic(brick.getID());
-        mqttService.publish(topic, ttnMqttPayloadBytes);
-        //System.out.printf("publish topic = \"%s\"\n", topic);
+        mqttService.publish(topic, payload);
+        System.out.printf("publish topic = \"%s\"\n", topic);
     }
 
     public static BleMqttProxy fromConfig(String configHost) {
