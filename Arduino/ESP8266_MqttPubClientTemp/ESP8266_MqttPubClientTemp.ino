115ecfa43d1ed795d87c68b261f55364b5aaaccd
tamberg <tamberg@yaler.net>
2021-12-30 16:03:20 +0100

    Replace client. Add TLS. Fix payload.

--- Arduino/ESP8266_MqttPubClientTemp/ESP8266_MqttPubClientTemp.ino
+++ Arduino/ESP8266_MqttPubClientTemp/ESP8266_MqttPubClientTemp.ino
@@ -1,5 +1,6 @@
-#include <ESP8266WiFi.h> // v2.4.2
-#include <ESP8266MQTTClient.h> // v1.0.4
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
 #include "DHTesp.h"
 
 const int dhtPin = 5; // Grove adapter I2C_1 or _2 used as D6
@@ -9,16 +10,12 @@ DHTesp dht;
 
 const char *ssid = "MY_SSID"; // TODO
 const char *password = "MY_PASSWORD"; // TODO
-const char *host = "mqtt://test.mosquitto.org"; // TODO
+const char *host = "test.mosquitto.org"; // TODO
+const int port = 8883;
 const char *topicStr = "bricks/0000-0001/actual"; // TODO
 
-MQTTClient client;
-volatile int connected = 0;
-
-void handleConnected() {
-  Serial.println("Connected to broker");
-  connected = 1;
-}
+BearSSL::WiFiClientSecure client;
+Adafruit_MQTT_Client mqtt(&client, host, port);
 
 void setup() {
   Serial.begin(115200);
@@ -32,14 +29,12 @@ void setup() {
   }
   Serial.print("Connected to network, local IP = "); 
   Serial.println(WiFi.localIP());
-
-  client.onConnect(handleConnected);
-  client.begin(host);
+  client.setInsecure(); // no cert validation
 }
 
 void loop() {
-  client.handle();
-  if (connected) {
+  //client.handle();
+  if (mqtt.connected()) {
     // Readings take about 250 ms, may be up to 2 s old
     float batt = 3.7; // V, TODO
     float humi = dht.getHumidity(); // %
@@ -48,16 +43,21 @@ void loop() {
       int b = batt * 100.0f;
       int h = humi * 100.0f;
       int t = temp * 100.0f;
-      char payload[] = { // TODO: fix "null bytes cut off payload"
+      uint8_t payload[] = {
         highByte(b), lowByte(b),
         highByte(h), lowByte(h),
-        highByte(t), lowByte(t),
-        '\0'
+        highByte(t), lowByte(t)
       };
-      printf("measure humi = %f, temp = %f\n", humi, temp);
+      printf("batt = %.2f, humi = %.2f, temp = %.2f\n", batt, humi, temp);
       printf("publish to %s\n", topicStr);
-      client.publish(topicStr, payload);
+      mqtt.publish(topicStr, payload, sizeof(payload));
     }
     delay(1000);
+  } else {
+    int result = mqtt.connect(); // calls client.connect()
+    if (result != 0) {
+      Serial.println(mqtt.connectErrorString(result));
+      delay(3000);
+    }
   }
 }
