204b5a97848f1e07fea1191b8a63c94ead1e2ba6
tamberg <tamberg@yaler.net>
2021-12-30 20:42:11 +0100

    Add Arduino code.

---
+++ Arduino/ESP8266_MqttSubClientBuzzer/ESP8266_MqttSubClientBuzzer.ino
@@ -0,0 +1,62 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+const char *ssid = "MY_SSID"; // TODO
+const char *password = "MY_PASSWORD"; // TODO
+const char *host = "test.mosquitto.org"; // TODO
+const int port = 8883;
+const char *topicStr = "bricks/0000-0006/target"; // TODO
+const int buzzerPin = 5;
+
+BearSSL::WiFiClientSecure client;
+Adafruit_MQTT_Client mqtt(&client, host, port);
+Adafruit_MQTT_Subscribe topic(&mqtt, topicStr);
+
+void handleMessage(char *buf, uint16_t len) {
+  Serial.print("handleMessage(), len = ");
+  Serial.println(len);
+  Serial.print('"');
+  Serial.print((char) buf[0]);
+  Serial.print((char) buf[1]);
+  Serial.println('"');
+  //printBuffer((uint8_t *) buf, len);
+  if (len == 2 && buf[0] == 0x01) {
+    digitalWrite(buzzerPin, HIGH);
+  } else {
+    digitalWrite(buzzerPin, LOW);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.print("\nConnecting to network ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100); // keeps watchdog happy
+  }
+  Serial.print("Connected to network, local IP = "); 
+  Serial.println(WiFi.localIP());
+  client.setInsecure(); // no cert validation
+  topic.setCallback(handleMessage);
+  mqtt.subscribe(&topic);
+}
+
+void loop() {
+  if (mqtt.connected()) {
+    Serial.println("Connected (still)");
+    mqtt.processPackets(10000); // ms, calls callbacks
+    if (!mqtt.ping()) {
+      mqtt.disconnect();  
+    }
+  } else {
+    int result = mqtt.connect(); // calls client.connect()
+    if (result != 0) {
+      Serial.println(mqtt.connectErrorString(result));
+      delay(3000);
+    }
+  }
+}
---
+++ Arduino/ESP8266_MqttSubClientTM1637/ESP8266_MqttSubClientTM1637.ino
@@ -0,0 +1,77 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <TM1637.h>
+
+#define CLK 5 // Grove D6
+#define DIO 4 // Grove D7
+
+TM1637 tm1637(CLK, DIO);
+
+const char *ssid = "MY_SSID"; // TODO
+const char *password = "MY_PASSWORD"; // TODO
+const char *host = "test.mosquitto.org"; // TODO
+const int port = 8883;
+const char *topicStr = "bricks/0000-0005/target"; // TODO
+const int buzzerPin = 5;
+
+BearSSL::WiFiClientSecure client;
+Adafruit_MQTT_Client mqtt(&client, host, port);
+Adafruit_MQTT_Subscribe topic(&mqtt, topicStr);
+
+void displayValue(float f) {
+  int a = f;
+  int a1 = a / 10;
+  int a0 = a % 10;
+  int b = f * 100.0f;
+  b = b % 100;
+  int b1 = b / 10;
+  int b0 = b % 10;
+  tm1637.point(POINT_ON); // or POINT_OFF
+  tm1637.display(0, a1);
+  tm1637.display(1, a0);
+  tm1637.display(2, b1);
+  tm1637.display(3, b0);
+}
+
+void handleMessage(char *buf, uint16_t len) {
+  if (len == 2) {
+    short i = (buf[0] << 8) | (buf[1] << 0);
+    float f = i / 100.0f;
+    displayValue(f);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  tm1637.set(BRIGHT_TYPICAL); // or BRIGHT_DARKEST, ...
+  tm1637.init();
+  Serial.print("\nConnecting to network ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100); // keeps watchdog happy
+  }
+  Serial.print("Connected to network, local IP = "); 
+  Serial.println(WiFi.localIP());
+  client.setInsecure(); // no cert validation
+  topic.setCallback(handleMessage);
+  mqtt.subscribe(&topic);
+}
+
+void loop() {
+  if (mqtt.connected()) {
+    Serial.println("Connected (still)");
+    mqtt.processPackets(10000); // ms, calls callbacks
+    if (!mqtt.ping()) {
+      mqtt.disconnect();  
+    }
+  } else {
+    int result = mqtt.connect(); // calls client.connect()
+    if (result != 0) {
+      Serial.println(mqtt.connectErrorString(result));
+      delay(3000);
+    }
+  }
+}
