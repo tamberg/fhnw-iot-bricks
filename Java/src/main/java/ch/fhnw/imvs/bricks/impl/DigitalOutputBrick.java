f9717dc1fdb2a8c0a37f493fb1ef1efb9775db60
tamberg <tamberg@yaler.net>
2022-01-02 13:13:36 +0100

    Update.

--- Java/src/main/java/ch/fhnw/imvs/bricks/impl/DigitalOutputBrick.java
+++ Java/src/main/java/ch/fhnw/imvs/bricks/impl/DigitalOutputBrick.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 FHNW, Switzerland. All rights reserved.
+// Copyright (c) 2022 FHNW, Switzerland. All rights reserved.
 // Licensed under MIT License, see LICENSE for details.
 
 package ch.fhnw.imvs.bricks.impl;
