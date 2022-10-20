0e566eaee232652e1a9291d45ca9459e65c4df57
Thomas Amberg <tamberg@yaler.net>
2022-10-20 11:49:40 +0200

    Update ImageTest.java

--- Java/src/test/ImageTest.java
+++ Java/src/test/ImageTest.java
@@ -10,10 +10,10 @@ class ImageTest {
 	public static void main(String[] args) {
     	Image image;
       try {
-	    	//InputStream in = new URL("https://fablabzurich-prusa-0.try.yaler.io/?action=snapshot").openStream();
+	    	//InputStream in = new URL("https://LOCAL_IP/?action=snapshot").openStream();
 	  		// Files.copy(in, Paths.get(FILE_NAME), StandardCopyOption.REPLACE_EXISTING);    	
 	    	//InputStreamReader r = new InputStreamReader(in);
-  			//URL url = new URL("https://admin:f8bl8bf8bl8b@fablabzurich-prusa-0.try.yaler.io/?action=snapshot");
+  			//URL url = new URL("https://LOCAL_IP/?action=snapshot");
         URL url = new URL("https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Felis_silvestris_silvestris.jpg/1200px-Felis_silvestris_silvestris.jpg");
         image = ImageIO.read(url);
   		} catch (IOException e) {
