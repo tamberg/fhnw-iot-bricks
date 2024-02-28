d0ce7a3b04c230c16442bc398f6aee40dc62d58c
tamberg <tamberg@yaler.net>
2024-02-28 12:15:52 +0100

    Update.

--- Java/setup.sh
+++ Java/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir target
 mkdir lib
 curl -Lo lib/minimal-json-0.9.5.jar https://github.com/ralfstx/minimal-json/releases/download/0.9.5/minimal-json-0.9.5.jar &&
 curl -Lo lib/org.eclipse.paho.client.mqttv3-1.2.3.jar https://repo.eclipse.org/content/repositories/paho-releases/org/eclipse/paho/org.eclipse.paho.client.mqttv3/1.2.3/org.eclipse.paho.client.mqttv3-1.2.3.jar &&
