d64fbb009d3eeabf4ecfbb08300d992782529355
tamberg <tamberg@yaler.net>
2024-02-28 17:33:03 +0100

    Update.

--- Java/src/main/java/ch/fhnw/imvs/bricks/sensors/CameraBrick.java
+++ Java/src/main/java/ch/fhnw/imvs/bricks/sensors/CameraBrick.java
@@ -1,9 +1,9 @@
-// Copyright (c) 2022 FHNW, Switzerland. All rights reserved.
+// Copyright (c) 2023 FHNW, Switzerland. All rights reserved.
 // Licensed under MIT License, see LICENSE for details.
 
 package ch.fhnw.imvs.bricks.sensors;
 
-import java.awt.*;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -12,12 +12,8 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
-import javax.imageio.IIOImage;
+import java.nio.file.Paths;
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
-import javax.imageio.ImageWriter;
-import javax.imageio.stream.ImageOutputStream;
 
 import ch.fhnw.imvs.bricks.core.Proxy;
 import ch.fhnw.imvs.bricks.core.Brick;
@@ -37,32 +33,14 @@ public final class CameraBrick extends Brick {
     protected byte[] getTargetPayload(boolean mock) {
         byte[] payload = null;
         if (mock) {
-            ByteArrayOutputStream byteArrayStream = new ByteArrayOutputStream();
-            //ImageOutputStream imageStream = null;
             try {
-                BufferedImage image = ImageIO.read(new File("test.jpg"));
-                ImageIO.write(image, "jpg", byteArrayStream);
-                //imageStream = ImageIO.createImageOutputStream(byteArrayStream);
-                //ImageWriter jpgWriter = ImageIO.getImageWritersByFormatName("JPEG").next();
-                //ImageWriteParam jpgWriteParam = jpgWriter.getDefaultWriteParam();
-                //jpgWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-                //jpgWriteParam.setCompressionQuality(0.6f);
-                //jpgWriter.setOutput(imageStream);
-                //IIOImage iioImage = new IIOImage(currentImage, null, null);
-                //jpgWriter.write(null, iioImage, jpgWriteParam);
-                //jpgWriter.dispose();
-                byte[] imageBytes = byteArrayStream.toByteArray();
+                Path filePath = Paths.get("test.jpg");
+                byte[] imageBytes = Files.readAllBytes(filePath);
                 ByteBuffer buf = ByteBuffer.allocate(imageBytes.length);
+                // TODO: battery, length
                 buf.put(imageBytes);
                 payload = buf.array();
-            } catch (IOException e) {
-                // give up
-            } finally {
-                try {
-                    //imageStream.close();
-                    byteArrayStream.close();
-                } catch (IOException e) {} // give up
-            }
+            } catch (IOException e) {} // give up
         }
         return payload;
     }
